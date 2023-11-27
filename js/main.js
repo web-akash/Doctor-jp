0800a2c6026162a429dd46a567a3d21d2d412a5a
sakkhor mjmdr <you@example.com>
2023-11-27 21:26:31 +0600

    button add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
-$(document).ready(function () {
-  $(".navbar-toggler").on("click", function () {
-    $("#sidebar").slideToggle();
+// $(document).ready(function () {
+//   $(".navbar-toggler").on("click", function () {
+//     $("#sidebar").slideToggle();
+//   });
+// });
+
+// Tushar v1.0.0
+
+$(function () {
+  $(".banner-main").slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // prevArrow: '<i class="fa-solid fa-arrow-left left-arrow"></i>',
+    // nextArrow: '<i class="fa-solid fa-arrow-right right-arrow"></i>',
+    pauseOnHover: false,
+    arrows: false,
+    fade: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  });
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+
+    if (scrolling > 150) {
+      $(".extra-button").fadeIn(200);
+    } else {
+      $(".extra-button").fadeOut(200);
+    }
+    $(".extra-btn-top").click(function () {
+      $("html,body").animate({
+        scrollTop: 0,
+      });
+    });
+    // if (scrolling > 1500) {
+    //   $(".extra-button").fadeOut(1);
+    // }
   });
 });
